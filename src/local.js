93dfb5b067efbd195961698a1def30e9627bc3a5
Omar Abdelazim <omar-abdelazim-dev@users.noreply.github.com>
2026-09-08 19:11:18 +0300

    fix: isolate local server from Vercel runtime

---
+++ src/local.js
@@ -0,0 +1,2 @@
+import { createApp } from './app.js';
+createApp().listen(Number(process.env.PORT ?? 3000), () => console.log('Arbitrage Tracker API running.'));
--- src/server.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from './app.js';
-const app = createApp();
-if (!process.env.VERCEL) app.listen(Number(process.env.PORT ?? 3000), () => console.log('Arbitrage Tracker API running.'));
-export default app;
